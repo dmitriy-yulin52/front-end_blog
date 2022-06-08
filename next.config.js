1e8b9b7fd8e195ddfc0d6af17a63e9f2419e74a6
yulin dima <yulin_dimon@mail.ru>
2022-06-08 14:01:11 +0300

    fix config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    images: {
-        domains: ['leonardo.osnova.io'],
-    },
-
 }
 
 module.exports = nextConfig
